8492c601b732019e3dc47cd6749f136fbaa59d23
Nicole Graiff <nicole.graiff@gmail.com>
2020-07-31 23:30:30 -0400

    Added add department function

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Define dependencies
+//Define module dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
@@ -11,7 +11,7 @@ const infoStyle = chalk.yellowBright;
 
 //Create connection
 const connection = mysql.createConnection({
-    host: "localhost" || process.env.PORT,
+    host: 'localhost' || process.env.PORT,
 
     port: process.env.PORT,
 
@@ -19,10 +19,33 @@ const connection = mysql.createConnection({
 
     password: process.env.PASSWORD,
 
-    //database = "employee_DB"
+    database: 'employee_DB'
 });
 
-//After connection is made, console.log application into and summary
+//Function to quit out of the application when "Exit Application" is selected
+const endApplication = () => {
+  console.log(welcomeStyle('We hope you were able to take care of everything you needed.','\n','2 cents before you go: “Strive not to be a success, but rather to be of value.” – Albert Einstein'));
+  connection.end();
+};
+
+
+//** ! SERVER FUNCTIONS ! **//
+  const addDepartment = () => {
+    console.log('The add department function has been entered.');
+    inquirer.prompt({
+        name: 'newDepartment',
+         type: 'input',
+         message: 'What is the name of the department that you would like to add?'
+    }).then((answers) => {
+        //Test 
+        connection.query("INSERT INTO department SET ?", {name: answers.newDepartment});
+        console.log(infoStyle(`Success! Your new department has been added to the database`));
+        runRequest();
+    });
+};
+
+
+//After connection is made, console.log application info and summary
 //Start function containing inquirer prompts
 connection.connect(function(err) {
     if (err) throw err;
@@ -44,7 +67,8 @@ connection.connect(function(err) {
     runRequest();
   });
   
-  function runRequest() {
+  //Initial Inquirer Prompts: select add, view, update, or exit
+  const runRequest = () => {
     inquirer
       .prompt({
         name: "action",
@@ -53,7 +77,8 @@ connection.connect(function(err) {
         choices: [
           "Add information",
           "View information",
-          "Update information"
+          "Update information",
+          "Exit application"
         ]
       })
       .then(function(answer) {
@@ -69,6 +94,10 @@ connection.connect(function(err) {
         case "Update information":
           updateInfo();
           break;
+
+        case "Exit application":
+          endApplication();
+          break;
         }
       });
   }
@@ -82,41 +111,109 @@ connection.connect(function(err) {
           type: "rawlist",
           message: "What information would you like to add?",
           choices: [
-            "Department information",
-            "Role information",
-            "Employee information"
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "I changed my mind. Go back to the start"
             ]
         }
       ])
       .then(function(answer) {
-        console.log(infoStyle(`OK. You would like to add: ${answer.category}`));
+        console.log(infoStyle(`OK. You would like to: ${answer.category}`));
 
         switch (answer.category) {
-          case "Department information":
+          case "Add a Department":
             addDepartment();
             break;
     
-          case "Role information":
+          case "Add a Role":
             addRole();
             break;
     
-          case "Employee information":
+          case "Add an Employee":
             addEmployee();
+            break;
+
+          case "I changed my mind. Go back to the start":
+            runRequest();
+            break;
+          }
+          
+        });
+  };
+  
+  function viewInfo() {
+      //console.log('View info was selected.');
+      console.log(infoStyle('OK, you would like to view some information.'));
+      inquirer
+      .prompt([
+        {
+          name: "category",
+          type: "rawlist",
+          message: "What information would you like to view?",
+          choices: [
+            "View all Departments",
+            "View all Roles",
+            "View all Employees",
+            "I changed my mind. Go back to the start"
+            ]
+        }
+      ])
+      .then(function(answer) {
+        console.log(infoStyle(`OK. You would like to: ${answer.category}`));
+
+        switch (answer.category) {
+          case "Add a Department":
+            viewDepartments();
+            break;
+    
+          case "Add a Role":
+            viewRoles();
+            break;
+    
+          case "Add an Employee":
+            viewEmployees();
+            break;
+
+          case "I changed my mind. Go back to the start":
+            runRequest();
             break;
           }
         });
   };
   
-  function viewInfo() {
-      console.log('View info was selected.');
-  };
-  
   function updateInfo() {
       console.log('Update info was selected.');
-  };
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+      console.log(infoStyle('OK, you would like to update some information.'));
+      inquirer
+      .prompt([
+        {
+          name: "category",
+          type: "rawlist",
+          message: "What information would you like to update?",
+          choices: [
+            "Update employee role",
+            "Update employee manager",
+            "I changed my mind. Go back to the start"
+            ]
+        }
+      ])
+      .then(function(answer) {
+        console.log(infoStyle(`OK. You would like to: ${answer.category}`));
+
+        switch (answer.category) {
+          case "Update employee role":
+            updateEmployeeRole();
+            break;
+    
+          case "Update employee manager":
+            updateEmployeeManager();
+            break;
+    
+          case "I changed my mind. Go back to the start":
+            runRequest();
+            break;
+          }
+        });
+  };
